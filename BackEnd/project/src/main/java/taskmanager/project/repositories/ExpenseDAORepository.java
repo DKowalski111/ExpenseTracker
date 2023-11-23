2df37c15fda3edb871f4c44f55efd13b3ebdb532
Dawid Kowalski <91938265+DKowalski111@users.noreply.github.com>
2023-11-24 00:49:43 +0100

    Delete BackEnd/project/src/main/java/taskmanager/project directory

--- BackEnd/project/src/main/java/taskmanager/project/repositories/ExpenseDAORepository.java
+++
@@ -1,7 +0,0 @@
-package taskmanager.project.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import taskmanager.project.models.Expense;
-
-interface ExpenseDAORepository extends JpaRepository<Expense, Long> {
-}
--- BackEnd/project/src/main/java/taskmanager/project/services/ExpenseService.java
+++
@@ -1,9 +0,0 @@
-package taskmanager.project.services;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@Transactional
-public class ExpenseService {
-}
